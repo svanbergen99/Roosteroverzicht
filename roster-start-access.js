01c9fdce1e044c5a3834b0be13ca3da921e05619
svanbergen99 <s.vanbergen99@gmail.com>
2026-09-03 13:56:36 +0200

    Bump private roster asset version for workers view

--- roster-start-access.js
+++ roster-start-access.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const VERSION = "20260903-66";
+  const VERSION = "20260903-68";
   const app = document.getElementById("app");
   const searchCard = document.querySelector(".search-card");
   const detachedAuthTrigger = document.getElementById("continueButton");
