5978861560b77dd2570cfb90db62571562b52bfe
svanbergen99 <s.vanbergen99@gmail.com>
2026-09-04 02:21:37 +0200

    Load revised weather and birthday setup

--- agenda-export.js
+++ agenda-export.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const VERSION = "20260904-115";
+  const VERSION = "20260904-116";
   const FILE_MONTHS = ["Januari","Februari","Maart","April","Mei","Juni","Juli","Augustus","September","Oktober","November","December"];
   const MONTH_FILE_RE = /^Roosterindex_(Januari|Februari|Maart|April|Mei|Juni|Juli|Augustus|September|Oktober|November|December)\.json$/i;
 
@@ -155,6 +155,5 @@
   loadScript("roster-start-access.js");
   loadScript("public-portal.js");
   loadScript("start-weather.js");
-  loadScript("weather-netherlands-fallback.js");
   loadScript("roster-home-button.js");
 })();
