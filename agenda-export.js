3264c8a8557df63f16ff92f05229c3fa168e79aa
svanbergen99 <s.vanbergen99@gmail.com>
2026-09-05 20:56:11 +0200

    Bump asset cache for Kibana embed test

--- agenda-export.js
+++ agenda-export.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const VERSION = "20260905-140";
+  const VERSION = "20260905-141";
   const FILE_MONTHS = ["Januari","Februari","Maart","April","Mei","Juni","Juli","Augustus","September","Oktober","November","December"];
   const MONTH_FILE_RE = /^Roosterindex_(Januari|Februari|Maart|Mei|Juni|Juli|Augustus|September|Oktober|November|December|April)\.json$/i;
 
